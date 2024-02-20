526e888007c529707eda3f549917bebbf8de7a61
hemanthnvd <hemanthnvd@gmail.com>
2024-02-19 21:41:34 -0500

    added workflow file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_network" "vpc_network" {
-  name = var.vpc_name
-  auto_create_subnetworks = false
-  routing_mode = var.vpc_routing_mode
+  name                            = var.vpc_name
+  auto_create_subnetworks         = false
+  routing_mode                    = var.vpc_routing_mode
   delete_default_routes_on_create = true
 }
 resource "google_compute_subnetwork" "network1" {
@@ -17,8 +17,8 @@ resource "google_compute_subnetwork" "network2" {
   network       = google_compute_network.vpc_network.id
 }
 resource "google_compute_route" "default" {
-  name = var.default_route_name
-  dest_range  = var.default_route_dest_range
-  network     = google_compute_network.vpc_network.id
+  name             = var.default_route_name
+  dest_range       = var.default_route_dest_range
+  network          = google_compute_network.vpc_network.id
   next_hop_gateway = "default-internet-gateway"
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,32 @@
 variable "gcp_credentials" {
+  type = string
 }
 variable "gcp_project" {
+  type = string
 }
 variable "gcp_region" {
+  type = string
 }
 variable "vpc_name" {
+  type = string
 }
 variable "vpc_routing_mode" {
+  type = string
 }
-variable "subnetwork1_name" { 
+variable "subnetwork1_name" {
+  type = string
 }
 variable "subnetwork1_ip_cidr_range" {
+  type = string
 }
-variable "subnetwork2_name" { 
+variable "subnetwork2_name" {
+  type = string
 }
 variable "subnetwork2_ip_cidr_range" {
 }
 variable "default_route_name" {
+  type = string
 }
-variable "default_route_dest_range" {  
+variable "default_route_dest_range" {
+  type = string
 }
